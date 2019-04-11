51372e987d030b77d68d449564a1c7b64150244f
nyawuma <etiennepatricknyawuma@yahoo.fr>
2019-04-11 11:00:58 +0100

    ok

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,22 +1,26 @@
-const API_TOKEN ="77f0405b2b9e6ad1484d4551041ad42a";
+// API/TMDBApi.js
+
+const API_TOKEN = "77f0405b2b9e6ad1484d4551041ad42a";
 
 export function getFilmsFromApiWithSearchedText (text, page) {
-  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + 
-  '&page=' + page
-
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
   return fetch(url)
-  .then((response)=> response.json())
-  .catch((error)=> console.error(error))
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
 }
 
-export function getImageFromApi(name){
+export function getFilmDetailFromApi (id) {
+  return fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + API_TOKEN + '&language=fr')
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+}
+
+export function getBestFilmsFromApi (page) {
+  return fetch('https://api.themoviedb.org/3/discover/movie?api_key=' + API_TOKEN + '&vote_count.gte=1000&sort_by=release_date.desc&language=fr&page=' + page)
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+}
+
+export function getImageFromApi (name) {
   return 'https://image.tmdb.org/t/p/w300' + name
-}
-
-export function getFilmDetailFromApi(id){
-
-  const url = 'https://api.themoviedb.org/3/movie/' + id + '?api_key=' + API_TOKEN + '&language=fr'
-    return fetch(url)
-    .then((response)=> response.json())
-    .catch((error)=> console.error(error))
 }
--- Store/configureStore.js
+++ Store/configureStore.js
@@ -1,6 +1,14 @@
-import { createStore } from 'redux';
+// Store/configureStore.js
+
+import { createStore } from 'redux'
 import toggleFavorite from './Reducers/favoriteReducer'
+import setAvatar from './Reducers/avatarReducer'
+import { persistCombineReducers } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+const rootPersistConfig = {
+  key: 'root',
+  storage: storage
+}
 
-
-export default createStore(toggleFavorite)
+export default createStore(persistCombineReducers(rootPersistConfig, {toggleFavorite, setAvatar}))
